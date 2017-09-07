541cbf31d7480f8491b38ae9d741e7a56c0f6dc5
Ben McGinnis <bmcginnis@yext.com>
2017-09-07 17:40:27 -0400

    functional and allows custom parsers

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 const postcss = require('postcss');
-const dss = require('postcss');
+const dss = require('dss');
+const fs = require('fs');
 
-module.exports = postcss.plugin('postcss-dss', function() {
-  return function(css) {
+function runDSS(rawCSS) {
+    return new Promise((resolve) => {
+        dss.parse(rawCSS, {}, (result) => {
+            resolve(result);
+        });
+    });
+}
 
-  };
+function saveResults(result, filename) {
+    return new Promise((resolve, reject) => {
+        let strung = JSON.stringify(result, null, 2);
+        fs.writeFile(filename, strung, null, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+module.exports = postcss.plugin('postcss-dss', (options) => {
+    for (let parser of options.parsers || []) {
+        dss.parser(parser.atRule, parser.func);
+    }
+    return function (root) {
+        return runDSS(root.toString()).then((res) => {
+            return saveResults(res, options.fileName);
+        });
+    };
 });
--- index.test.js
+++ index.test.js
@@ -1,11 +1,85 @@
 const postcss = require('postcss');
+const fs = require('fs');
 const plugin = require('./');
+const testdataDir = 'testdata';
+
+if (!fs.existsSync(testdataDir)) {
+    fs.mkdirSync(testdataDir);
+}
 
 function run(input, output, opts) {
-  return postcss([plugin(opts)])
+    return postcss([plugin(opts)])
     .process(input)
     .then(result => {
-      expect(result.css).toEqual(output);
-      expect(result.warnings().length.toBe(0));
+        let out = fs.readFileSync(opts.fileName, 'utf-8');
+        expect(out).toEqual(output);
+        expect(result.warnings().length).toBe(0);
     });
 }
+
+
+it('parses DSS comments and saves them to specified output file', () => {
+    let input = `/*
+* @name Button
+* @description Your standard form button.
+*
+* @state :hover - Highlights when hovering.
+* @state .smaller - A smaller button
+*
+* @markup
+*   <button>This is a button</button>
+*/`;
+    let output = `{
+  "blocks": [
+    {
+      "name": "Button",
+      "description": "Your standard form button.",
+      "state": [
+        [
+          {
+            "name": ":hover",
+            "escaped": "pseudo-class-hover",
+            "description": "Highlights when hovering."
+          }
+        ],
+        [
+          {
+            "name": ".smaller",
+            "escaped": "smaller",
+            "description": "A smaller button"
+          }
+        ]
+      ],
+      "markup": [
+        {
+          "example": "   <button>This is a button</button>",
+          "escaped": "   &lt;button&gt;This is a button&lt;/button&gt;"
+        }
+      ]
+    }
+  ]
+}`;
+
+    return run(input, output, { fileName: 'testdata/basic_styles.json' });
+});
+
+
+it('accepts custom parsers', () => {
+    return run('/* @version 1.0 */', `{
+  "blocks": [
+    {
+      "version": "1.0"
+    }
+  ]
+}`, {
+    fileName: 'testdata/custom_parser.json',
+    parsers: [
+        {
+            atRule: 'version',
+            func: function () {
+                return this.line.contents;
+            }
+        }
+    ]
+});
+});
